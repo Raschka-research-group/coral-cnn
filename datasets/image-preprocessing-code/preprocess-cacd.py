726e54579db008d9c16868fa76b2292b9dec9fbc
Sebastian Raschka <mail@sebastianraschka.com>
2021-05-05 11:04:45 -0500

    Update to print software version and output

--- datasets/image-preprocessing-code/preprocess-cacd.py
+++ datasets/image-preprocessing-code/preprocess-cacd.py
@@ -4,13 +4,33 @@ import os
 import numpy as np
 from PIL import Image
 
+print(f'DLIB: {dlib.__version__}')
+print(f'NumPy: {np.__version__}')
+print(f'OpenCV: {cv2.__version__}')
+
+# Last tested with
+# DLIB: 19.22.0
+# NumPy: 1.20.2
+# OpenCV: 4.5.2
+
+
 root_path = './'
+
+orig_path = os.path.join(root_path, 'CACD2000')
+out_path = os.path.join(root_path, 'CACD2000-centered')
+
+if not os.path.exists(orig_path):
+    raise ValueError(f'Original image path {orig_path} does not exist.')
+
+if not os.path.exists(out_path):
+    os.mkdir(out_path)
 
 detector = dlib.get_frontal_face_detector()
 keep_picture = []
 
-for picture_name in os.listdir(root_path + 'CACD2000/'):
-    img = cv2.imread(root_path + 'CACD2000/' + picture_name)
+
+for picture_name in os.listdir(orig_path):
+    img = cv2.imread(os.path.join(orig_path, picture_name))
 
     detected = detector(img, 1)
 
@@ -46,7 +66,9 @@ for picture_name in os.listdir(root_path + 'CACD2000/'):
         try:
             tmp = np.array(Image.fromarray(np.uint8(tmp)).resize((120, 120), Image.ANTIALIAS))
 
-            cv2.imwrite(root_path + 'CACD2000-centered/' + picture_name, tmp)
+            cv2.imwrite(os.path.join(out_path, picture_name), tmp)
+            print(f'Wrote {picture_name}')
             keep_picture.append(picture_name)
         except ValueError:
+            print(f'Failed {picture_name}')
             pass
